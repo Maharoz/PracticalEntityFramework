6215987c89f36f125dfb8af143fa6ab5986cef15
Maharoz <mahruj66@gmail.com>
2021-01-23 21:42:17 +0600

    codes after session two

---
+++ PartTwo/Activity0302_EFCoreNewDb/InventoryDatabaseCore/InventoryDbContext.cs
@@ -0,0 +1,39 @@
+﻿using InventoryModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace InventoryDatabaseCore
+{
+    public class InventoryDbContext:DbContext
+    {
+        private static IConfigurationRoot _configuration;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // optionsBuilder.UseSqlServer("Data Source = localhost;Initial Catalog=InventoryManager; Integrated Security=True");
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true,
+                    reloadOnChange: true);
+
+                _configuration = builder.Build();
+
+                var cnstr = _configuration.GetConnectionString("InventoryManager");
+                optionsBuilder.UseSqlServer(cnstr);
+
+            }
+        }
+        public DbSet<Item> Items { get; set; }
+        public InventoryDbContext():base()
+        {
+
+        }
+        public InventoryDbContext(DbContextOptions options):base()
+        {
+            
+        }
+    }
+}
